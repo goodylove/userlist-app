e17abe92261c7c5a2ae70ab7226c6b08b767da37
goodness <goodyc474@gmail.com>
2023-04-19 13:54:49 +0100

    ft-added-search-bar-functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import UserDetails from "./components/UserDetails";
 import Nav from "./components/nav";
 import Loading from "./components/Loading";
 
-const LazyLoading = React.lazy(() => import("./pages/Home"));
+// const LazyLoading = React.lazy(() => import("./pages/Home"));
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      <Loading />;
+    }, 5000);
+  }, []);
   return (
     <>
       <Nav />
       <div className="   flex justify-center items-center w-full ">
         <Routes>
-          <Route
-            index
-            element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyLoading />
-              </React.Suspense>
-            }
-          />
+          <Route index element={<Home />} />
           <Route path="users/:id" element={<UserDetails />} />
         </Routes>
       </div>
